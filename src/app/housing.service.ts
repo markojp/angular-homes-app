bdcf6fd53bbfc24bda0e62fe2e2f11c69d3c6969
markojp <joakim.forthewin@outlook.com>
2024-07-14 23:20:13 +0200

    Added form to submit application and basic submit of application data to service. Part 7

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housinglocation';
+import { last } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -117,5 +118,9 @@ export class HousingService {
     return this.housingLocationList.find(
       (housingLocation) => housingLocation.id === id
     );
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
   }
 }
