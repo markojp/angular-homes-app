2fc372b94adb8508e949281c785d6fa667f7dcc1
markojp <joakim.forthewin@outlook.com>
2024-07-14 22:58:26 +0200

    Added Housing Service and used service for Home and Details component. Details component template completed. Part 6

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, Inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -11,9 +13,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
 }
